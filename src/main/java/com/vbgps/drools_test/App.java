20e205b9152613fddd9140855d15644c65f6130f
yebing <yebing@vvsmark.com>
2018-11-26 15:05:01 +0800

    update

--- src/main/java/com/vbgps/drools_test/App.java
+++ src/main/java/com/vbgps/drools_test/App.java
@@ -17,16 +17,35 @@ public class App {
 		KieSession kieSession = kieContainer.newKieSession("simpleRuleKSession");
 
 		Person p1 = new Person();
-		p1.setAge(20);
-		Car car = new Car();
-		car.setPerson(p1);
-
-		kieSession.insert(car);
-
-		int count = kieSession.fireAllRules();
-
-		System.out.println(count);
-		System.out.println(car.getDiscount());
+		p1.setAge(10);
+		Car car1 = new Car();
+		car1.setPerson(p1);
+		kieSession.insert(car1);	
+		
+		Person p2 = new Person();
+		p2.setAge(50);
+		Car car2 = new Car();
+		car2.setPerson(p2);
+		kieSession.insert(car2);
+		
+		Person p3 = new Person();
+		p3.setAge(70);
+		Car car3 = new Car();
+		car3.setPerson(p3);
+		kieSession.insert(car3);
+		
+		Person p4 = new Person();
+		p4.setAge(85);
+		Car car4 = new Car();
+		car4.setPerson(p4);
+		kieSession.insert(car4);
+		
+		
+		System.out.println(kieSession.fireAllRules());
+		System.out.println(car1.getDiscount());
+		System.out.println(car2.getDiscount());
+		System.out.println(car3.getDiscount());
+		System.out.println(car4.getDiscount());
 
 		kieSession.dispose();
 	}
