8f649083e661f3e1d2f02dfca33835cd58ad005a
yebing <yebing@vvsmark.com>
2018-11-26 11:40:51 +0800

    update

--- src/main/java/com/vbgps/drools_test/App.java
+++ src/main/java/com/vbgps/drools_test/App.java
@@ -1,13 +1,33 @@
 package com.vbgps.drools_test;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		KieServices kieServices = KieServices.Factory.get();
+		// 默认自动加载 META-INF/kmodule.xml
+		KieContainer kieContainer = kieServices.getKieClasspathContainer();
+		// kmodule.xml 中定义的 ksession name
+		KieSession kieSession = kieContainer.newKieSession("all-rules");
+
+		Person p1 = new Person();
+		p1.setAge(20);
+		Car car = new Car();
+		car.setPerson(p1);
+
+		kieSession.insert(car);
+
+		int count = kieSession.fireAllRules();
+
+		System.out.println(count);
+		System.out.println(car.getDiscount());
+
+		kieSession.dispose();
+	}
 }
